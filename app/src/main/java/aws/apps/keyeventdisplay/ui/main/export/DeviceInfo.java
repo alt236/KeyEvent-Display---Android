5bfb68e0262f9a177330eecc57b7cd71c9e1cb9c
Alexandros Schillings <aschillings@gmail.com>
2018-09-26 16:35:01 +0100

    Tidied up the device info extraction

--- app/src/main/java/aws/apps/keyeventdisplay/ui/main/export/DeviceInfo.java
+++ app/src/main/java/aws/apps/keyeventdisplay/ui/main/export/DeviceInfo.java
@@ -1,43 +1,31 @@
 package aws.apps.keyeventdisplay.ui.main.export;
 
+import android.content.Context;
+
+import java.util.Map;
+
+import aws.apps.keyeventdisplay.deviceinfo.DeviceInfoCollator;
+
 /*package*/ final class DeviceInfo {
 
-    public static void collectDeviceInfo(final StringBuilder sb) {
-        append(sb, "OS Release: ", android.os.Build.VERSION.RELEASE, "\n");
-        append(sb, "OS API Level: ", android.os.Build.VERSION.SDK_INT, "\n");
-        append(sb, "Board: ", android.os.Build.BOARD, "\n");
-        append(sb, "Brand: ", android.os.Build.BRAND, "\n");
-        append(sb, "CPU_ABI: ", android.os.Build.CPU_ABI, "\n");
-        append(sb, "CPU_ABI2: ", android.os.Build.CPU_ABI2, "\n");
-        append(sb, "Device: ", android.os.Build.DEVICE, "\n");
-        append(sb, "Display: ", android.os.Build.DISPLAY, "\n");
-        append(sb, "Fingerprint: ", android.os.Build.FINGERPRINT, "\n");
-        append(sb, "Host: ", android.os.Build.HOST, "\n");
-        append(sb, "ID: ", android.os.Build.ID, "\n");
-        append(sb, "Manufacturer: ", android.os.Build.MANUFACTURER, "\n");
-        append(sb, "Model: ", android.os.Build.MODEL, "\n");
-        append(sb, "Product: ", android.os.Build.PRODUCT, "\n");
-        append(sb, "Tags: ", android.os.Build.TAGS, "\n");
-        append(sb, "Type: ", android.os.Build.TYPE, "\n");
-        append(sb, "User: ", android.os.Build.USER, "\n");
-        append(sb, "Bootloader: ", android.os.Build.BOOTLOADER, "\n");
-        append(sb, "Hardware: ", android.os.Build.HARDWARE, "\n");
-        append(sb, "Radio: ", android.os.Build.RADIO, "\n");
+    private DeviceInfoCollator deviceInfoCollator;
+
+    DeviceInfo(Context context) {
+        this.deviceInfoCollator = new DeviceInfoCollator(context);
     }
 
-    private static void append(final StringBuilder sb,
-                               final String val1,
-                               final String val2,
-                               final String val3) {
-        sb.append(val1);
-        sb.append(val2);
-        sb.append(val3);
+    public void collectDeviceInfo(final StringBuilder sb) {
+        final Map<String, String> info = deviceInfoCollator.collectDeviceInfo();
+
+        for (final String key : info.keySet()) {
+            append(sb, key + ": ", info.get(key), "\n");
+        }
     }
 
-    private static void append(final StringBuilder sb,
-                               final String val1,
-                               final int val2,
-                               final String val3) {
+    private void append(final StringBuilder sb,
+                        final String val1,
+                        final String val2,
+                        final String val3) {
         sb.append(val1);
         sb.append(val2);
         sb.append(val3);
